14efbb3d02a76c2e5b9be2ce766810600ee43779
Andrei Lucaci <contact@andreilucaci.ro>
2019-03-29 23:42:31 +0200

    first commit

---
+++ KillApp/ViewModels/DelegateCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace KillApp.ViewModels
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> _callback;
+        private readonly Action _singleCallback;
+
+        public DelegateCommand(Action callback)
+        {
+            _singleCallback = callback;
+        }
+
+        public DelegateCommand(Action<object> callback)
+        {
+            _callback = callback;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_callback != null)
+            {
+                _callback(parameter);
+            }
+            else
+            {
+                _singleCallback();
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ KillApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using KillApp.Annotations;
+
+namespace KillApp.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Process> _processes;
+        private int _numberOfProcesses;
+
+        public ObservableCollection<Process> Processes
+        {
+            get => _processes;
+            set
+            {
+                _processes = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Processes)));
+            }
+        }
+
+        public ICommand RowClick { get; }
+
+        public ICommand Refresh { get; }
+
+        public Process SelectedProcess { get; set; }
+
+        public int NumberOfProcesses
+        {
+            get => _numberOfProcesses;
+            set
+            {
+                _numberOfProcesses = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NumberOfProcesses)));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            RowClick = new DelegateCommand(KillProcess);
+            Refresh = new DelegateCommand(LoadProcesses);
+
+            Processes = new ObservableCollection<Process>(); 
+
+            LoadProcesses();
+        }
+
+        private void KillProcess()
+        {
+            if (SelectedProcess != null)
+            {
+                try
+                {
+                    Processes.Where(x => x.ProcessName == SelectedProcess.ProcessName).ToList().ForEach(x => x.Kill());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                Thread.Sleep(100);
+
+                LoadProcesses();
+            }
+        }
+
+        private void LoadProcesses()
+        {
+            Processes = new ObservableCollection<Process>(Process.GetProcesses().OrderBy(x => x.ProcessName).ToList());
+            NumberOfProcesses = Processes.Count;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
